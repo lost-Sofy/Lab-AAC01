ae2385bd251146824448784e8c2a73b07bd047e8
Samael509 <pavel718234@gmail.com>
2026-09-17 11:05:15 +0300

    Lab-AAC01: first programm with Environment

--- Documents/AAC-Egorova/Lab-AAC01/Program.cs
+++ Documents/AAC-Egorova/Lab-AAC01/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Компьютер: {Environment.MachineName}");
+Console.WriteLine($"Пользователь: {Environment.UserName}");
+Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM.yyyy HH:mm}");
+
